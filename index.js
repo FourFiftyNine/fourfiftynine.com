818de132929b93609cc424db82959c6c855fa335
Anthony Sessa <sessa@fourfiftynine.com>
2012-05-24 11:30:34 -0400

    updated enviroment to run at new linode ip

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ if(!process.env.NODE_ENV) process.env.NODE_ENV="local"
 
 var app = require('./config/app')();
 var port = process.env.PORT || 3000;
-
-app.listen(port);
+var host = app.set('host');
+app.listen(port, host);
 
 console.log('\x1b[36mFourFiftyNine.com\x1b[90m v%s\x1b[0m running as \x1b[1m%s\x1b[0m on http://%s:%d',
   app.set('version'),
