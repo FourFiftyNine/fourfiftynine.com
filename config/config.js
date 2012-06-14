09afdf8bfaeacf41b116346def55c46bd8f80717
Anthony Sessa <sessa@fourfiftynine.com>
2012-06-14 18:24:16 -0400

    updated compression for css with tmp folder as well

--- config/config.js
+++ config/config.js
@@ -66,32 +66,35 @@ module.exports = function(app){
     app.set('version', '0.0.1');
   });
 
-  // Compress JavaScript
-  // Using UglifyJS
-  new compressor.minify({
-      type: 'uglifyjs',
-      fileIn: [
-        'public/javascript/libs/jquery.form.js',
-        'public/javascript/libs/cycle/jquery.cycle.all.js',
-        'public/javascript/plugins.js',
-        'public/javascript/main.js', 
-      ],
-      fileOut: 'public/javascript/all.min.js',
-      tempPath: 'tmp/',
-      callback: function(err){
-          // console.log(err);
-      }
-  });
+  app.configure(function() {
+    // Compress JavaScript
+    // Using UglifyJS
+    new compressor.minify({
+        type: 'uglifyjs',
+        fileIn: [
+          'public/javascript/libs/jquery.form.js',
+          'public/javascript/libs/cycle/jquery.cycle.all.js',
+          'public/javascript/plugins.js',
+          'public/javascript/main.js', 
+        ],
+        fileOut: 'public/javascript/all.min.js',
+        tempPath: 'tmp/',
+        callback: function(err){
+            // console.log(err);
+        }
+    });
 
-  new compressor.minify({
-      type: 'yui-css',
-      fileIn: 'public/stylesheets/style.css',
-      fileOut: 'public/stylesheets/compressed.css',
-      tempPath: 'tmp/',
-      callback: function(err){
-          // console.log(err);
-      }
-  });
+    new compressor.minify({
+        type: 'yui-css',
+        fileIn: 'public/stylesheets/style.css',
+        fileOut: 'public/stylesheets/compressed.css',
+        tempPath: 'tmp/',
+        callback: function(err){
+            // console.log(err);
+        }
+    });
+  })
+
   // // Left Off here
   // app.configure(function () {
   //   this
